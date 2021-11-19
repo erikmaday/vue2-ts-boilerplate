1aab08ff92d648d6e335fa4f14111797c5ab47df
Erik Maday <erik@charterup.com>
2021-11-19 11:36:00 -0500

    feat(CUPOPS-2): added some more styles

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,6 +14,7 @@
         <h1 class="display-2 font-weight-bold mb-3">
           The Future Home of CharterUp for Operators
         </h1>
+        <v-text-field outlined>Test Button</v-text-field>
       </v-col>
     </v-row>
   </v-container>

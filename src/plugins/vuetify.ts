934514ac6f66912f665d3f03ba43f86eb618e2a2
Erik Maday <erik@charterup.com>
2021-11-19 10:26:13 -0500

    feat(CUPOPS-2): establish Vuetify variables

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,15 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
+import colors from "@/scss/colors.scss";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        ...colors,
+      },
+    },
+  },
+});

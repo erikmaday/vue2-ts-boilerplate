2021683ea5007b01ed7c26a7c40167853c0706ec
Josh Steverson <joshsteverson@Joshs-MacBook-Pro.local>
2021-11-19 18:28:20 -0500

    feat(CUPOPS-6): adding basic http service

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,24 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue from 'vue'
+import { Address, Stop } from './models/dto'
+import { HttpService } from './services/HttpService'
 
 export default Vue.extend({
-  name: "App",
+  name: 'App',
 
   data: () => ({
     //
   }),
-});
+  async mounted() {
+    const httpService: HttpService = new HttpService()
+    const a = {} as unknown as Address
+    const fakeStop = { orderIndex: 1, active: true, address: a }
+    const response = await httpService
+      .post<Address, Stop>('http://something', fakeStop)
+      .catch((f) => f.data)
+    console.log(response?.data?.street1 || 'nothing to see here')
+  },
+})
 </script>
---
+++ src/services/HttpService.ts
@@ -0,0 +1,18 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+export class HttpService {
+  post<T, P>(
+    url: string,
+    payload: P,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<T>> {
+    return axios.post<P, AxiosResponse<T>>(url, payload, config)
+  }
+
+  get<T, P>(
+    url: string,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<T>> {
+    return axios.get<P, AxiosResponse<T>>(url, config)
+  }
+}

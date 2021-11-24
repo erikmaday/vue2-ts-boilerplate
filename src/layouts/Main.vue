1b2645b1cc0f2e934c350f8295e9d040d56ded2b
Erik Maday <erik@charterup.com>
2021-11-23 22:04:57 -0500

    feat(CUPOPS-3): added main layout

---
+++ src/layouts/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container
+    fluid
+    class="pa-8 fill-height align-start"
+    :style="{ 'max-width': `${$vuetify.breakpoint.thresholds.md}px` }"
+  >
+    <slot />
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component()
+export default class Main extends Vue {}
+</script>

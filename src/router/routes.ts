e8f68b457fe6360d9c9648a4ce36ac192f0103a7
Erik Maday <erik@charterup.com>
2021-11-23 23:56:58 -0500

    feat(CUPOPS-3): added map with panel layout

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -10,14 +10,14 @@ export const routes: Array<RouteConfig> = [
         name: 'home',
         component: () => import('@/views/Home.vue'),
         children: [
-          // {
-          //   path: 'sidenav',
-          //   name: 'sidenav',
-          //   component: () =>
-          //     import(
-          //       /* webpackChunkName: "sidenav" */ '@/views/SideNavTest.vue'
-          //     ),
-          // },
+          {
+            path: 'map',
+            name: 'map',
+            component: () =>
+              import(
+                /* webpackChunkName: "map" */ '@/layouts/MapWithSidebar.vue'
+              ),
+          },
         ],
       },
     ],

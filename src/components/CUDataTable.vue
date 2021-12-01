834fc2874287592671b331a08d94fc1377044d53
Peter Lam <peter.lam@charterup.com>
2021-12-01 16:16:09 -0500

    chore(CUPOPS-10): Convert To Class Component

--- src/components/CUDataTable.vue
+++ src/components/CUDataTable.vue
@@ -10,50 +10,58 @@
         <CUDataTableActionColumn :actions="actions" />
       </template>
       <template #item.phone="{ item }">
-
+        <a :href="`tel:${item.phone}`">
+          {{ phoneFormatFilter(String(item.phone)) }}
+        </a>
+      </template>
+      <template #item.email="{ item }">
+        <a :href="`mailto:${item.email}`">{{ item.email }}</a>
       </template>
     </v-data-table>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Vue, Component, Prop } from 'vue-property-decorator'
 import CUDataTableActionColumn from '@/components/CUDataTableActionColumn.vue'
+import { phoneFormatFilter } from '@/utils/phone'
+import { ActionColumn } from '@/models/ActionColumn'
 
-export default Vue.extend({
-  props: {
-    actions: {
-      type: Array,
-      default: undefined,
-    },
-  },
-  components: {
-    CUDataTableActionColumn,
-  },
-  data() {
-    return {
-      headers: [
-        { text: 'Actions', value: 'actions' },
-        { text: 'Name', value: 'name' },
-        { text: 'Email', value: 'email' },
-        { text: 'Phone', value: 'phone' },
-        { text: 'Type', value: 'type' },
-      ],
-      items: [
-        {
-          name: 'Peter Lam',
-          email: 'peter.lam@charterup.com',
-          phone: 9783539016,
-          type: 'Driver',
-        },
-        {
-          name: 'Erik Maday',
-          email: 'erikm@charterup.com',
-          phone: 1231231233,
-          type: 'Admin',
-        },
-      ],
-    }
-  },
+@Component({
+  components: { CUDataTableActionColumn },
 })
+export default class CUDataTable extends Vue {
+  @Prop({
+    type: Array,
+    required: false,
+    default: undefined,
+  })
+  actions!: Array<ActionColumn>
+
+  phoneFormatFilter = phoneFormatFilter
+
+  // TEmp data for testing
+  headers = [
+    { text: 'Actions', value: 'actions' },
+    { text: 'Name', value: 'name' },
+    { text: 'Email', value: 'email' },
+    { text: 'Phone', value: 'phone' },
+    { text: 'Type', value: 'type' },
+  ]
+
+  items = [
+    {
+      name: 'Peter Lam',
+      email: 'peter.lam@charterup.com',
+      phone: 9783539016,
+      type: 'Driver',
+    },
+    {
+      name: 'Erik Maday',
+      email: 'erikm@charterup.com',
+      phone: 1231231233,
+      type: 'Admin',
+    },
+  ]
+}
 </script>

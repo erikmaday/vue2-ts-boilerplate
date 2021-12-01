c6e8aa9225e3173cef1b5f466e4a8b8740a9fd5e
Erik Maday <erik@charterup.com>
2021-12-01 18:18:18 -0500

    feat(CUPOPS-11): added authentication to the app, configured bearer token on auth, added temporary login page

---
+++ src/utils/env.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const hostPrefix = (prefix: string) => (prefix ? `${prefix}.` : '')
+
+export const apiBaseUrl = (prefix = 'api'): string => {
+  let host
+  if (process.env.VUE_APP_BASE_URL_OVERRIDE) {
+    host = process.env.VUE_APP_BASE_URL_OVERRIDE
+  } else {
+    const hostSubdomain = window.location.host.split(':')[0].split('.')[0]
+    if (hostSubdomain === 'localhost') {
+      host = 'dev.coachrail.com'
+    } else {
+      host = hostSubdomain + '.coachrail.com'
+    }
+  }
+  axios.defaults.baseURL = `https://${hostPrefix(prefix)}${host}`
+  return `${hostPrefix(prefix)}${host}`
+}

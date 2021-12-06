233344e32d5474a9f55cb98a5f731bdc53a92369
Erik Maday <erik@charterup.com>
2021-12-06 11:25:26 -0500

    fix(CUPOPS-17): removed test code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,6 @@
     <TheSideBar />
     <TheAppBar />
     <v-main>
-      <AutocompleteAddress v-model="address" />
       <router-view />
     </v-main>
   </div>
@@ -13,19 +12,15 @@
 import { Component, Vue, Watch } from 'vue-property-decorator'
 import TheAppBar from '@/components/TheAppBar.vue'
 import TheSideBar from '@/components/TheSideBar.vue'
-import AutocompleteAddress from '@/components/AutocompleteAddress.vue'
-import { Address } from '@/models/dtos'
 import modules from '@/store/modules'
 
 @Component({
   components: {
     TheAppBar,
     TheSideBar,
-    AutocompleteAddress,
   },
 })
 export default class Home extends Vue {
-  address: Address | null = null
   @Watch('$vuetify.breakpoint.mdAndUp')
   shouldHideSidebar(value: boolean): void {
     if (value) {

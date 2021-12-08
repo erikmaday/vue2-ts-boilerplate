aea1c1ac3e1ac8d51eba4cb94dbcd728ee427d89
Erik Maday <erik@charterup.com>
2021-12-07 19:03:21 -0500

    feat(CUPOPS-21): create reusable trip assignment icons

--- src/models/dto/index.ts
+++ src/models/dto/index.ts
@@ -2,11 +2,23 @@ export * from './ApiResult'
 
 export * from './Address'
 export * from './AddressApiResult'
+<<<<<<< Updated upstream
+=======
+export * from './DriverAssignment'
+export * from './PaymentMethod'
+export * from './PaymentType'
+export * from './Place'
+export * from './Reservation'
+>>>>>>> Stashed changes
 export * from './SpotTime'
 export * from './Stop'
 export * from './Trip'
 export * from './Vehicle'
+<<<<<<< Updated upstream
 export * from './PaymentMethod'
 export * from './PaymentType'
 export * from './Place'
 export * from './User'
+=======
+export * from './VehicleAssignment'
+>>>>>>> Stashed changes

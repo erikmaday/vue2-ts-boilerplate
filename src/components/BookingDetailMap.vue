85b1e58f0f3f4db2ac2ab730b1f2f0a8e4c66ced
Erik Maday <erik@charterup.com>
2021-12-14 17:27:12 -0500

    feat(CUPOPS-22): implemented vue2-google-maps

---
+++ src/components/BookingDetailMap.vue
@@ -0,0 +1,29 @@
+<template>
+  <GmapMap
+    ref="mapRef"
+    :center="center"
+    :zoom="7"
+    :options="options"
+    class="w-full h-240"
+  />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { mapStyles } from '@/data/mapStyles'
+@Component
+export default class BookingDetailMap extends Vue {
+  map = null
+  center = { lat: 10, lng: 10 }
+  bounds = null
+  options = {
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    fullscreenControl: true,
+    disableDefaultUi: true,
+    styles: mapStyles,
+  }
+}
+</script>
---
+++ src/plugins/gmaps.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+// FIXME: env?
+const API_KEY = 'AIzaSyCe-36GFHdejJC2VVLYiHcfZBI9fBi37Tg'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: API_KEY,
+    installComponents: true,
+  },
+})

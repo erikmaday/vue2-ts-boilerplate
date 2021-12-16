2b1fbe163bc49b92b6c814ab6e7a0d3879fe183b
Erik Maday <erik@charterup.com>
2021-12-16 14:23:56 -0500

    establishing branch

---
+++ src/components/CUTimeline.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="cu-timeline">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Provide, Vue } from 'vue-property-decorator'
+
+@Component
+export default class CUTimeline extends Vue {
+  @Provide() isInTimeline = true
+}
+</script>
---
+++ src/components/CUTimelineItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="cu-timeline-item">
+    <div class="background-primary h-24 w-24">Icon</div>
+    <div class="background-purple h-60 w-2">Line</div>
+    <div>Content</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Inject, Vue } from 'vue-property-decorator'
+
+@Component
+export default class CUTimelineItem extends Vue {
+  @Inject({ default: false }) isInTimeline: boolean
+}
+</script>

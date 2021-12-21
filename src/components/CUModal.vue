4a940b47cd77e6688fb632b12c89e3e47c7aad95
Calvin Gee <calvin@charterup.com>
2021-12-21 10:28:08 -0800

    feat(CUPOPS-40): Create new modal

---
+++ src/components/CUModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-dialog
+    v-model="isDialogOpen"
+    :fullscreen="$vuetify.breakpoint.xs"
+    width="500px"
+    max-width="500px"
+  >
+    <v-card>
+      <v-card-title class="background-primary text-white font-medium">
+        <slot name="title"></slot>
+      </v-card-title>
+      <v-card-text>
+        <slot name="text"></slot>
+      </v-card-text>
+      <v-card-actions>
+        <slot name="buttons"></slot>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { Prop, Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class CUModal extends Vue {
+  @Prop({
+    type: Boolean,
+    required: false,
+    default: false,
+  })
+  isDialogOpen!: boolean
+}
+</script>

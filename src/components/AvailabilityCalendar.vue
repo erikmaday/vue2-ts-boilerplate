6ba0cf055954523258297e20a5ab994948011774
Peter Lam <peter.lam@charterup.com>
2022-01-06 19:22:35 -0500

    feat(CUPOPS-65): Establishing Calendar Headers - gray out current day on calendar - display date in dayheader

---
+++ src/components/AvailabilityCalendar.vue
@@ -0,0 +1,57 @@
+<template>
+  <CalendarView :show-date="showDate" display-period-uom="week">
+    <template #dayHeader="{ index, label }">
+      <div 
+        class="cv-week-day-header" 
+        style="flex-basis: calc(100% / 7);"
+        :class="{
+          'background-gray-mid-light border-width-1 border-style-1 border-color-gray-mid-light':
+            isHeaderToday(index.charAt(3)),
+        }"
+      >
+        <p>{{ label }}</p>
+        <p>{{ getDayHeaderDate(index.charAt(3)) }}</p>
+      </div>
+    </template>
+  </CalendarView>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import { CalendarView } from 'vue-simple-calendar'
+import dayjs from 'dayjs'
+@Component({
+  components: { CalendarView },
+})
+export default class AvailabilityCalendar extends Vue {
+  @Prop({
+    required: true,
+    type: Date,
+  })
+  showDate!: Date
+
+  getDayHeaderDate(index: number): number {
+    return dayjs(this.showDate).startOf('week').add(index, 'day').date()
+  }
+
+  isHeaderToday(index: number) {
+    const headerDate = dayjs(this.showDate).startOf('week').add(index, 'day')
+    console.log("> comparing:", headerDate.toString())
+    return dayjs().isSame(headerDate, 'day')
+  }
+}
+</script>
+<style lang="scss" scoped>
+.cv-wrapper {
+  .cv-week-day-header {
+    padding: 2px 8px;
+  }
+}
+
+::v-deep .cv-day-number {
+  display: none;
+}
+
+::v-deep .cv-day.today {
+  background: $gray-mid-light;
+}
+</style>
--- src/views/Availability.vue
+++ src/views/Availability.vue
@@ -1,6 +1,28 @@
 <template>
   <Main>
     <h1>Availability</h1>
+    <p></p>
+    <v-row align="start">
+      <v-btn
+        class="border-radius-2"
+        x-small
+        icon
+        color="primary"
+        @click="shiftCalendarDisplayDate(-1)"
+      >
+        <CUIcon width="20px" height="20px">keyboard_arrow_left</CUIcon>
+      </v-btn>
+      <AvailabilityCalendar :show-date="calendarDisplayDateAsJS" />
+      <v-btn
+        class="border-radius-2"
+        x-small
+        icon
+        color="primary"
+        @click="shiftCalendarDisplayDate(1)"
+      >
+        <CUIcon width="20px" height="20px">keyboard_arrow_right</CUIcon>
+      </v-btn>
+    </v-row>
   </Main>
 </template>
 
@@ -11,8 +33,9 @@ import availability from '@/services/availability'
 import dayjs from 'dayjs'
 import { AvailabilityGetRequest } from '@/models/dto/Availability'
 import { Reservation } from '@/models/dto'
+import AvailabilityCalendar from '@/components/AvailabilityCalendar.vue'
 @Component({
-  components: { Main },
+  components: { Main, AvailabilityCalendar },
 })
 export default class Availability extends Vue {
   loadedDates = {
@@ -21,14 +44,29 @@ export default class Availability extends Vue {
   }
 
   reservations: Reservation[] = []
+  calendarDisplayDate = dayjs()
+
+  get calendarDisplayDateAsJS(): Date {
+    return this.calendarDisplayDate.toDate()
+  }
+
+  get calendarDisplayDateMonth(): string {
+    return this.calendarDisplayDate.format()
+  }
 
   async mounted(): Promise<void> {
     this.reservations = await this.getDispatchDataForDates(this.loadedDates)
   }
 
-  async getDispatchDataForDates(dates: AvailabilityGetRequest): Promise<Reservation[]> {
+  async getDispatchDataForDates(
+    dates: AvailabilityGetRequest
+  ): Promise<Reservation[]> {
     const res = await availability.getData(dates)
     return res.data.reservations
+  }
+
+  shiftCalendarDisplayDate(numWeeks: number): void {
+    this.calendarDisplayDate = this.calendarDisplayDate.add(numWeeks, 'week')
   }
 }
 </script>

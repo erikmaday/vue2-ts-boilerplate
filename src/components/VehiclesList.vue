cae71f839cdff74063b35b67bf4fa31fa284824a
Erik Maday <erik@charterup.com>
2022-01-07 12:58:42 -0500

    fixing build errors

--- src/components/VehiclesList.vue
+++ src/components/VehiclesList.vue
@@ -25,17 +25,39 @@ import { RawLocation } from 'vue-router'
 })
 export default class VehiclesList extends Vue {
   columns: DataTableColumn[] = [
-    { text: 'Name', value: 'vehicleName' },
-    { text: 'Type', value: 'vehicleTypeName' },
-    { text: 'License Plate', value: 'licensePlate' },
-    { text: 'Garage', value: 'garageName' },
     {
+      _t_id: '53a7edf7-93a6-45bb-8ba0-12acfd68a2ce',
+      text: 'Name',
+      value: 'vehicleName',
+    },
+    {
+      _t_id: 'b03d27dc-fbad-453f-a98d-83904311f791',
+      text: 'Type',
+      value: 'vehicleTypeName',
+    },
+    {
+      _t_id: 'f2d23cb5-a14e-4dab-b747-d5fe937c8d26',
+      text: 'License Plate',
+      value: 'licensePlate',
+    },
+    {
+      _t_id: 'eec52a62-8881-4fc0-a5a9-9f69fc35eec4',
+      text: 'Garage',
+      value: 'garageName',
+    },
+    {
+      _t_id: 'c10e1fda-b1a4-4b8e-934d-908d13a9a26d',
       text: 'Make',
       value: 'vehicleMake',
       computedText: (row): string =>
         `${row.vehicleYear} ${row.vehicleMake} ${row.vehicleModel} - ${row.passengerCapacity} pax`,
     },
-    { text: '', value: 'actions', type: 'actions' },
+    {
+      _t_id: '89e4030c-f2ed-4ea5-a468-79370b1eb0b1',
+      text: '',
+      value: 'actions',
+      type: 'actions',
+    },
   ]
 
   tableView = vehicle.tableView

aa15ba13f2eac48f5e1601eba06a72d99e93aa72
Peter Lam <peter.lam@charterup.com>
2022-01-11 11:37:42 -0500

    fix(CUPOPS-81): Allow for global dayjs without typecasting - On my end, I'm still getting a Veter error, but not getting a TypeScript error when accessing this.$dayjs. Not sure if this is happening on other people's machines

--- src/plugins/dayjs.ts
+++ src/plugins/dayjs.ts
@@ -10,10 +10,17 @@ dayjs.extend(timezone)
 dayjs.extend(localeData)
 dayjs.extend(advancedFormat)
 
-export default Object.defineProperties(Vue.prototype, {
-  $dayjs: {
-    get() {
-      return dayjs
-    },
-  },
-})
+Vue.prototype.$dayjs = dayjs
+// import Vue from 'vue';
+// import dayjs from 'dayjs';
+// import 'dayjs/locale/en';
+
+// import relativeTime from 'dayjs/plugin/relativeTime';
+// import LocalizedFormat from 'dayjs/plugin/localizedFormat';
+// import utc from 'dayjs/plugin/utc';
+
+// dayjs.locale(process.env.VUE_APP_I18N_LOCALE);
+// dayjs.extend(relativeTime);
+// dayjs.extend(LocalizedFormat);
+// dayjs.extend(utc);
+// Vue.prototype.$dayjs = dayjs;

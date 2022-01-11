c480d965dbe71f8e4fb20839c29d8a9b92a9e188
Peter Lam <peter.lam@charterup.com>
2022-01-11 11:44:16 -0500

    chore(CUPOPS-81): Removing Comment

--- src/plugins/dayjs.ts
+++ src/plugins/dayjs.ts
@@ -11,16 +11,3 @@ dayjs.extend(localeData)
 dayjs.extend(advancedFormat)
 
 Vue.prototype.$dayjs = dayjs
-// import Vue from 'vue';
-// import dayjs from 'dayjs';
-// import 'dayjs/locale/en';
-
-// import relativeTime from 'dayjs/plugin/relativeTime';
-// import LocalizedFormat from 'dayjs/plugin/localizedFormat';
-// import utc from 'dayjs/plugin/utc';
-
-// dayjs.locale(process.env.VUE_APP_I18N_LOCALE);
-// dayjs.extend(relativeTime);
-// dayjs.extend(LocalizedFormat);
-// dayjs.extend(utc);
-// Vue.prototype.$dayjs = dayjs;

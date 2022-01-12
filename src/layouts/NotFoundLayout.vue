e16535cb6ac989ae5c140abad55179d36d6e61e3
John Lennon <JohnLennon@CharterUPs-MBP.lan>
2022-01-12 16:19:12 -0500

    fix(CUPOPS-91) more style fixes

--- src/layouts/NotFoundLayout.vue
+++ src/layouts/NotFoundLayout.vue
@@ -1,27 +1,30 @@
 <template>
-  <v-row align="stretch" justify="center" class="margin-bottom-11" no-gutters>
-    <v-col cols="7" class="text-center">
-      <v-img :src="img" class="margin-top-6 margin-bottom-3" />
-      <div class="font-40">
-        <slot name="header" />
-        <span v-if="!$slots.header">Oops! We took a wrong turn.</span>
+  <v-row align="stretch" justify="center" class="margin-b-11" no-gutters>
+    <v-col cols="10" sm="7" class="text-center">
+      <v-img :src="img" class="margin-t-6 margin-b-3" />
+      <div
+        :class="{
+          'font-40': $vuetify.breakpoint.smAndUp,
+          'font-30': $vuetify.breakpoint.xs,
+        }"
+      >
+        <slot name="header">Oops! We took a wrong turn.</slot>
       </div>
       <v-col class="pb-5 pt-2">
-        <slot name="content" />
-        <span v-if="!$slots.content">
+        <slot name="content">
           This page does not exist or some other error has occurred.
           <br />
           Please check your url or return to the
           <router-link :to="{ href: '/' }">home page</router-link>
           .
-        </span>
+        </slot>
       </v-col>
     </v-col>
   </v-row>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop} from 'vue-property-decorator'
+import { Vue, Component, Prop } from 'vue-property-decorator'
 
 @Component
 export default class NotFoundLayout extends Vue {

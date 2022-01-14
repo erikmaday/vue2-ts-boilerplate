11349f5e5c7ebce17d3f21c8ea797d892a209809
Peter Lam <peter.lam@charterup.com>
2022-01-13 22:07:12 -0500

    refactor(CUPOPS-124): Simplifying Password Form

--- src/views/ProfilePassword.vue
+++ src/views/ProfilePassword.vue
@@ -1,12 +1,12 @@
 <template>
   <v-row no-gutters>
     <v-col cols="12">
-      <span v-if="error">Please correct errors and submit again</span>
-      <v-form ref="changePasswordForm" v-model="valid" :lazy-validation="true">
+      <v-form ref="changePasswordForm">
         <CUTextField
           label="Old Password"
           id="my-profile-password-text-old-password"
           v-model="formFields.oldPassword"
+          :error-messages="oldPasswordErrors"
           :rules="[(v) => !!v || 'Old password is required']"
           type="password"
           required
@@ -17,7 +17,7 @@
           id="my-profile-password-text-new-password"
           v-model="formFields.newPassword"
           :rules="[
-            (v) => !!v || 'New password is required',
+            (v) => !!v || 'Please enter a new password',
             (v) =>
               v.length >= 8 || 'New password must be at least 8 characters',
           ]"
@@ -30,7 +30,7 @@
           id="my-profile-password-text-confirm-new-password"
           v-model="formFields.confirmNewPassword"
           :rules="[
-            (v) => !!v || 'Confirm new password is required',
+            (v) => !!v || 'Please confirm your password',
             (v) =>
               v === this.formFields.newPassword || 'New passwords must match',
           ]"
@@ -38,18 +38,34 @@
           required
           outlined
         />
+        <div class="d-flex justify-end">
+          <v-btn
+            text
+            small
+            color="primary"
+            @click="$router.push({ name: 'profile' })"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            small
+            color="primary"
+            :disabled="loading || disabled"
+            @click="submit"
+          >
+            <template v-if="!loading">Change Password</template>
+            <span v-else class="padding-x-3">
+              <v-progress-circular
+                :size="10"
+                :width="2"
+                color="white"
+                indeterminate
+              />
+            </span>
+          </v-btn>
+        </div>
       </v-form>
     </v-col>
-    <v-btn
-      medium
-      :disabled="disabled"
-      :color="submitButtonOptions.color"
-      :loading="submitting"
-      class="min-w-232 text-white"
-      @click="submit"
-    >
-      {{ submitButtonOptions.text }}
-    </v-btn>
   </v-row>
 </template>
 
@@ -57,70 +73,46 @@
 import user from '@/services/user'
 import { Vue, Component, Watch } from 'vue-property-decorator'
 import auth from '@/store/modules/auth'
-import { ColoredMessage } from '@/models/ColoredMessage'
 
 @Component({})
 export default class ProfilePassword extends Vue {
-  valid = false
   formFields: Record<string, string> = {
     oldPassword: '',
     newPassword: '',
     confirmNewPassword: '',
   }
-  rules = []
-  success = false
-  error = false
-  submitting = false
-  disabled = true
+  loading = false
+  disabled = false
 
-  get submitButtonOptions(): ColoredMessage {
-    const color = this.success ? 'success' : this.error ? 'error' : 'primary'
-    const text = this.success
-      ? 'Password Changed!'
-      : this.error
-      ? 'Error Changing Password'
-      : 'Change Password'
-    return {
-      color: color,
-      text: text,
-    }
-  }
+  oldPasswordErrors = ''
+
   @Watch('formFields', { deep: true })
-  formFieldsChanged(): void {
-    this.success = false
-    this.error = false
-    this.disabled = this.isDisabled()
+  onFormUpdate(): void {
+    this.oldPasswordErrors = ''
+    this.disabled = false
   }
 
-  isDisabled(): boolean {
-    return !(
-      this.formFields.oldPassword &&
-      this.formFields.newPassword &&
-      this.formFields.confirmNewPassword
-    )
-  }
-  submit(): void {
-    this.$nextTick(async () => {
-      const form: any = this.$refs['changePasswordForm']
-      this.valid = form.validate()
-      if (this.valid) {
-        try {
-          this.error = false
-          this.submitting = true
-          await user.changePassword(
-            auth.getUserId,
-            this.formFields.newPassword,
-            this.formFields.oldPassword
-          )
-          this.success = true
-        } catch {
-          this.error = true
-        }
-      } else {
-        this.error = true
-      }
-      this.submitting = false
-    })
+  async submit(): Promise<void> {
+    this.loading = true
+    const form: any = this.$refs['changePasswordForm']
+
+    if (!form.validate()) {
+      return
+    }
+
+    user
+      .changePassword(
+        auth.getUserId,
+        this.formFields.newPassword,
+        this.formFields.oldPassword
+      )
+      .then(() => this.$router.push({ name: 'profile' }))
+      .catch((error) => {
+        this.disabled = true
+        this.oldPasswordErrors = error.response.data.description
+      })
+
+    this.loading = false
   }
 }
 </script>
@@ -128,8 +120,5 @@ export default class ProfilePassword extends Vue {
 <style lang="scss" scoped>
 .v-btn {
   transition: all 0.25s ease;
-}
-::v-deep .v-progress-circular {
-  color: white !important;
 }
 </style>

35c3b08dedd5c7ea2c76dee58738434952f8ca41
Calvin Gee <calvin@charterup.com>
2022-01-19 11:30:02 -0800

    feat(CUPOPS-193): No future dates predefined

--- src/data/predefined.ts
+++ src/data/predefined.ts
@@ -277,3 +277,137 @@ export const datePredefined: PredefinedFilter[] = [
     ],
   },
 ]
+
+export const noFutureDatesPredefined: PredefinedFilter[] = [
+  {
+    _t_id: 'b0ee285c-36c0-4b67-852f-4d4061c71632',
+    text: 'Range',
+    refreshOnSelect: false,
+    controls: [
+      {
+        _t_id: '33d667e1-7680-4121-87ae-adc33d9639ec',
+        text: 'Start',
+        filterType: 'gte',
+        value: 'Range Start',
+        displayValue: '',
+        recalculate: 'Range Start',
+      },
+      {
+        _t_id: 'f90720ee-688d-4a31-a7ae-cb28419fa7e7',
+        text: 'End',
+        filterType: 'lte',
+        value: 'Range End',
+        displayValue: '',
+        recalculate: 'Range End',
+      },
+    ],
+  },
+  {
+    _t_id: '5e7022f5-7d5b-4863-9f77-bf6247b98131',
+    text: 'Today',
+    refreshOnSelect: true,
+    controls: [
+      {
+        _t_id: '2b4e7209-743a-4162-800c-56544247deb8',
+        text: 'Start',
+        filterType: 'gte',
+        value: 'Today Start',
+        displayValue: '',
+        recalculate: 'Today Start',
+      },
+      {
+        _t_id: 'c4b47a97-d9ba-4c6f-8ce0-2e73f942f2e1',
+        text: 'End',
+        filterType: 'lte',
+        // hide: true,
+        value: 'Today End',
+        displayValue: '',
+        recalculate: 'Today End',
+      },
+    ],
+  },
+  {
+    _t_id: '4142b5dc-37fb-433a-a363-5ae6a3ef5967',
+    text: 'Last 7 days',
+    refreshOnSelect: true,
+    controls: [
+      {
+        _t_id: 'a8fdfff9-4e7d-42d9-b43a-05de2e7c72e9',
+        text: 'Start',
+        filterType: 'gte',
+        value: 'Last 7 Days Start',
+        displayValue: '',
+        recalculate: 'Last 7 Days Start',
+      },
+      {
+        _t_id: 'ca900e56-25b0-42bc-b97f-046a8b81d856',
+        text: 'End',
+        filterType: 'lte',
+        value: 'Last 7 Days End',
+        displayValue: '',
+        recalculate: 'Last 7 Days End',
+      },
+    ],
+  },
+  {
+    _t_id: '452f8663-083a-47d0-9578-e692a0ec3ce1',
+    text: 'This month',
+    refreshOnSelect: true,
+    controls: [
+      {
+        _t_id: 'c4a09705-2f69-4005-9aac-81eea3e2005d',
+        text: 'Start',
+        filterType: 'gte',
+        value: 'This Month Start',
+        displayValue: '',
+        recalculate: 'This Month Start',
+      },
+      {
+        _t_id: '2e548210-c9e3-4455-86f2-f2d6a6d42a4c',
+        text: 'End',
+        filterType: 'lte',
+        value: 'This Month End',
+        displayValue: '',
+        recalculate: 'This Month End',
+      },
+    ],
+  },
+  {
+    _t_id: 'fcb06d5a-a8db-425d-92fb-c54b543d3ff6',
+    text: 'Last month',
+    refreshOnSelect: true,
+    controls: [
+      {
+        _t_id: '7db5fb5b-70e6-447d-8879-fc35199363bc',
+        text: 'Start',
+        filterType: 'gte',
+        value: 'Last Month Start',
+        displayValue: '',
+        recalculate: 'Last Month Start',
+      },
+      {
+        _t_id: 'c4ec5fed-74ee-4ffd-903e-0782053a480c',
+        text: 'End',
+        filterType: 'lte',
+        value: 'Last Month End',
+        displayValue: '',
+        recalculate: 'Last Month End',
+      },
+    ],
+  },
+  {
+    _t_id: 'f1598e76-34ee-4cd2-964b-2c6b0eb018dd',
+    text: 'In the past',
+    refreshOnSelect: true,
+    controls: [
+      {
+        _t_id: '44c9f676-06c5-446f-9485-9713dcfcbec5',
+        text: 'Start',
+        filterType: 'lte',
+        value: 'In The Past',
+        displayValue: '',
+        recalculate: 'In The Past',
+      },
+    ],
+  },
+]

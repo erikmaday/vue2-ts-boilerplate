6c79b9846ce941801f713e1237385f7565fbe465
Calvin Gee <calvin@charterup.com>
2022-01-26 16:02:36 -0800

    feat(CUPOPS-208): Add sidebar routes to rates

--- src/views/Rates.vue
+++ src/views/Rates.vue
@@ -16,11 +16,15 @@ import MainWithSidebarNavigation from '@/components/MainWithSidebarNavigation.vu
 import RatesList from '@/views/RatesList.vue'
 import { SidebarLink } from '@/models/SidebarLink'
 import { toTitle } from '@/utils/string'
+import AdjustmentDetail from './AdjustmentDetail.vue'
+import AdjustmentsList from './AdjustmentsList.vue'
+
 @Component({
   components: {
     MainWithSidebarNavigation,
     MainWithSidebar,
     RatesList,
+    AdjustmentDetail,
   },
 })
 export default class CompanyRates extends Vue {
@@ -33,18 +37,46 @@ export default class CompanyRates extends Vue {
       component: RatesList,
       icon: 'rates',
     },
+    {
+      label: 'Adjustments',
+      href: '/adjustments',
+      name: 'rates.adjustments',
+      component: AdjustmentsList,
+      icon: 'swapVert',
+    },
+    {
+      label: 'Adjustments',
+      href: '/adjustments/edit/:id',
+      name: 'rates.adjustments.edit',
+      component: AdjustmentDetail,
+      hide: true,
+    },
+    {
+      label: 'Adjustments',
+      href: '/adjustments/add',
+      name: 'rates.adjustments.add',
+      component: AdjustmentDetail,
+      hide: true,
+    },
+    {
+      label: 'Adjustments',
+      href: '/adjustments/view/:id',
+      name: 'rates.adjustments.view',
+      component: AdjustmentDetail,
+      hide: true,
+    },
   ]
 
   get mode(): string {
-    if (this.$route.name) {
-      return this.$route.name
+    if (this.$route.name.includes('adjustments')) {
+      return 'rates.adjustments'
     }
-    return ''
+    return 'rates'
   }
 
   get companyComponent(): any {
     const link: SidebarLink | undefined = this.links.find(
-      (link) => link.name && link.name.includes(this.mode)
+      (link) => link.name && link.name.includes(this.$route.name)
     )
     if (link) {
       return link.component
